aa4ef839f010c7c283c1222212380bd6d55b7a24
Vidyesh CHURI <vidyesh95@gmail.com>
2020-10-21 23:49:45 +0530

    B

--- app/src/main/java/com/codinginflow/imagesearch/data/UnsplashApi.kt
+++ app/src/main/java/com/codinginflow/imagesearch/data/UnsplashApi.kt
@@ -1,14 +1,25 @@
 package com.codinginflow.imagesearch.data
 
+import com.codinginflow.imagesearch.BuildConfig
+import com.codinginflow.imagesearch.api.UnsplashResponse
+import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 /**
  * Code written by by vidyesh churi on 10/16/2020 2:08 PM
  */
 interface UnsplashApi {
+
+    companion object {
+        const val BASE_URL = "https://api.unsplash.com/"
+        const val CLIENT_ID = BuildConfig.UNSPLASH_ACCESS_KEY
+    }
+    @Headers("Accept-Version: v1", "Authorization: Client-ID $CLIENT_ID")
+    @GET("search/photos")
     suspend fun searchPhotos(
         @Query("query") query: String,
         @Query("page") page: Int,
         @Query("per_page") perPage: Int
-    )
+    ): UnsplashResponse
 }
