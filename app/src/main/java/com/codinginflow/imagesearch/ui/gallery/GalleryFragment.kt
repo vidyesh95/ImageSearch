380ab5e67e05a29d4408c4ae6c8d3d0fa4ae94be
Vidyesh CHURI <vidyesh95@gmail.com>
2020-10-27 23:30:48 +0530

    Recycler ongoing

--- app/src/main/java/com/codinginflow/imagesearch/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearch/ui/gallery/GalleryFragment.kt
@@ -1,10 +1,13 @@
 package com.codinginflow.imagesearch.ui.gallery
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.codinginflow.imagesearch.R
+import com.codinginflow.imagesearch.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -15,11 +18,31 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
     private val viewModel by viewModels<GalleryViewModel>()
 
+    private var _binding: FragmentGalleryBinding? = null
+
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        
         viewModel.photos.observe(viewLifecycleOwner) {
 
         }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentGalleryBinding.inflate(inflater, container, savedInstanceState)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
